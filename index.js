c384936f5fd0484678522eb8efd73815da89f7a8
Anthony Turetsky <tturetsky@gmail.com>
2018-05-11 13:27:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function takeANumber(katzDeli, name) {
-  katzDeli.push(name);
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+var numberRoller = 1;
+
+function takeANumber(katzDeli) {
+  numberRoller++;
+  katzDeli.push(numberRoller)
+  return `Your number is ${numberRoller}` 
+   
+
+  
 }
 
 function nowServing(deliLine) {
